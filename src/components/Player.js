e2a6fffb0bb6b4b9340216e2ae5549a8605622bd
hone1er <24376928+hone1er@users.noreply.github.com>
2021-12-31 14:00:21 -0800

    removed transaction code

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { GameContext } from "./GameProvider";
 import { PlayerDiv } from "../styledComponents/PlayerDiv";
-import { useWallet, useTransaction } from "@web3-ui/hooks";
+import { useWallet } from "@web3-ui/hooks";
 import { Address } from "@web3-ui/components";
 
 export default function Player(props) {
@@ -11,25 +11,12 @@ export default function Player(props) {
     connection.ens || connection.userAddress
   );
 
-  const { execute, loading, error } = useTransaction(
-    connection.signer.signMessage,
-    ["sign this"]
-  );
-
   useEffect(() => {
     setDisplayName(connection.ens || connection.userAddress);
-    console.log(connection);
+
     // eslint-disable-next-line
   }, [connection.userAddress, connection.ens]);
 
-  useEffect(() => {
-    async function exe() {
-      const [exe, load, err] = [await execute, await loading, await error];
-      console.log("EXE: ", connection.signer, await exe, load, err);
-    }
-    exe();
-    return () => {};
-  }, [execute, loading, error, connection.signer]);
   let display = displayName ? displayName : game.names[props.playerNumber];
   return (
     <PlayerDiv current={props.current}>
